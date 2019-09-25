c229c6ac8dc6be77eaacb6d00ddd3c3d0cddc14d
Hassan Iqbal <hassan_iqbal2812@outlook.com>
2019-09-25 23:45:52 +0000

    Added function to help soil sensor file

--- Soil_sensor.py
+++ Soil_sensor.py
@@ -1,9 +1,15 @@
 def Setup_Sensor():
     print("Seting up Sensor")
 
+def Acting_upon_data():
+    print("Acting on data")
+
+def Reading_data():
+    print("---Reading data---")
+    Acting_upon_data()        
 
 def main():
-	print("Start of script")
+    print("Start of script")
         
 
 if __name__== "__main__":
